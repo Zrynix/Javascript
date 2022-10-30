6f823942f8eaca2b71d259fda36acf31e4bbf3c8
Amine Aouragh <112860666+Zrynix@users.noreply.github.com>
2022-10-30 17:56:17 +0100

    Add files via upload

---
+++ 6.3/script 6.3.js
@@ -0,0 +1,7 @@
+function changeColor(){
+    document.getElementById("title").style.color = "beige";
+    document.getElementById("title").textContent = "Inhoud aangepast!";
+}
+function color_btn(){
+    changeColor();
+}
